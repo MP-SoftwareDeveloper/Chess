45a1d51c56ed66d1ea883030b88aa10b59a6ac30
MP <morteza.pourmehdi.se@gmailcom>
2025-04-27 08:54:54 +0100

    First background is prepared

--- Source.cpp
+++ Source.cpp
@@ -1,11 +1,288 @@
 #include <iostream>
 using namespace std;
-int main()
+void print()
 {
-	/*for (int i = 0; i < 256; i++)
+
 	{
-		cout << i <<" "<<char(i)<< endl;
-	}*/
-	
+		cout << char(218);
+		int aa = 7;
+		while (aa--)
+		{
+			int a = 4;
+			while (a--)
+			{
+				cout << char(196);
+			}
+			cout << char(194);
+		}
+		int a = 4;
+		while (a--)
+		{
+			cout << char(196);
+		}
+		cout << char(191);
+	}
+	cout << endl;
+	int b = 3;
+	while (b--)
+	{
+
+		{
+			int aa = 4;
+			cout << char(179);
+			while (aa--)
+			{
+				int a = 4;
+				while (a--)
+				{
+					cout << char(178);
+				}
+				cout << char(179);
+				a = 4;
+				while (a--)
+				{
+					cout << char(176);
+				}
+				cout << char(179);
+			}
+		}
+		cout << endl;
+		{
+			cout << char(179);
+			int aa = 4;
+			while (aa--)
+			{
+				cout << char(178) << "  " << char(178) << char(179);
+				cout << char(176) << "  " << char(176) << char(179);
+			}
+		}
+		cout << endl;
+		{
+			int aa = 4;
+			cout << char(179);
+			while (aa--)
+			{
+				int a = 4;
+				while (a--)
+				{
+					cout << char(178);
+				}
+				cout << char(179);
+				a = 4;
+				while (a--)
+				{
+					cout << char(176);
+				}
+				cout << char(179);
+			}
+		}
+		cout << endl;
+		{
+			cout << char(195);
+			int a = 7;
+			while (a--)
+				cout << char(196) << char(196) << char(196) << char(196) << char(197);
+			cout << char(196) << char(196) << char(196) << char(196) << char(180);
+		}
+		cout << endl;
+		{
+			int aa = 4;
+			cout << char(179);
+			while (aa--)
+			{
+				int a = 4;
+				while (a--)
+				{
+					cout << char(176);
+				}
+				cout << char(179);
+				a = 4;
+				while (a--)
+				{
+					cout << char(178);
+				}
+				cout << char(179);
+			}
+		}
+		cout << endl;
+		{
+			cout << char(179);
+			int aa = 4;
+			while (aa--)
+			{
+				cout << char(176) << "  " << char(176) << char(179);
+				cout << char(178) << "  " << char(178) << char(179);
+			}
+		}
+		cout << endl;
+		{
+			int aa = 4;
+			cout << char(179);
+			while (aa--)
+			{
+				int a = 4;
+				while (a--)
+				{
+					cout << char(176);
+				}
+				cout << char(179);
+				a = 4;
+				while (a--)
+				{
+					cout << char(178);
+				}
+				cout << char(179);
+			}
+		}
+		cout << endl;
+		{
+			cout << char(195);
+			int a = 7;
+			while (a--)
+				cout << char(196) << char(196) << char(196) << char(196) << char(197);
+			cout << char(196) << char(196) << char(196) << char(196) << char(180);
+		}
+		cout << endl;
+	}
+	{
+		int aa = 4;
+		cout << char(179);
+		while (aa--)
+		{
+			int a = 4;
+			while (a--)
+			{
+				cout << char(178);
+			}
+			cout << char(179);
+			a = 4;
+			while (a--)
+			{
+				cout << char(176);
+			}
+			cout << char(179);
+		}
+	}
+	cout << endl;
+	{
+		cout << char(179);
+		int aa = 4;
+		while (aa--)
+		{
+			cout << char(178) << "  " << char(178) << char(179);
+			cout << char(176) << "  " << char(176) << char(179);
+		}
+	}
+	cout << endl;
+	{
+		int aa = 4;
+		cout << char(179);
+		while (aa--)
+		{
+			int a = 4;
+			while (a--)
+			{
+				cout << char(178);
+			}
+			cout << char(179);
+			a = 4;
+			while (a--)
+			{
+				cout << char(176);
+			}
+			cout << char(179);
+		}
+	}
+	cout << endl;
+	{
+		cout << char(195);
+		int a = 7;
+		while (a--)
+			cout << char(196) << char(196) << char(196) << char(196) << char(197);
+		cout << char(196) << char(196) << char(196) << char(196) << char(180);
+	}
+	cout << endl;
+	{
+		int aa = 4;
+		cout << char(179);
+		while (aa--)
+		{
+			int a = 4;
+			while (a--)
+			{
+				cout << char(176);
+			}
+			cout << char(179);
+			a = 4;
+			while (a--)
+			{
+				cout << char(178);
+			}
+			cout << char(179);
+		}
+	}
+	cout << endl;
+	{
+		cout << char(179);
+		int aa = 4;
+		while (aa--)
+		{
+			cout << char(176) << "  " << char(176) << char(179);
+			cout << char(178) << "  " << char(178) << char(179);
+		}
+	}
+	cout << endl;
+	{
+		int aa = 4;
+		cout << char(179);
+		while (aa--)
+		{
+			int a = 4;
+			while (a--)
+			{
+				cout << char(176);
+			}
+			cout << char(179);
+			a = 4;
+			while (a--)
+			{
+				cout << char(178);
+			}
+			cout << char(179);
+		}
+	}
+	cout << endl;
+	{
+		cout << char(192);
+		int aa = 7;
+		while (aa--)
+		{
+			int a = 4;
+			while (a--)
+			{
+				cout << char(196);
+			}
+			cout << char(193);
+		}
+		int a = 4;
+		while (a--)
+		{
+			cout << char(196);
+		}
+		cout << char(217);
+	}
+	cout << endl;
 
 }
+	int main()
+	{
+		print();
+	}
+	/***** Trying to create square sape in console
+	int main()
+	{
+		cout << char(218) << char(196) << char(196) << char(196) << char(196) << char(191) << endl;
+		cout << char(179) << char(178) << char(178) << char(176) << char(176) << char(179) << endl;
+		cout << char(192) << char(196) << char(196) << char(196) << char(196) << char(217) << endl;
+
+	}**/
